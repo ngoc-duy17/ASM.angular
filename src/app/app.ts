57d0d779b9ad035cc39b76c1839a7ece0238a2df
NguyenNgocDuyPH32135 <duynnph32135@fpt.edu.vn>
2025-07-18 13:24:08 +0700

    RouterLink + sidebal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductList } from './product-list/product-list';
-import { CategoryList } from './category-list/category-list';
-import { ProductDetail } from './product-detail/product-detail';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList, CategoryList, ProductDetail],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -10,9 +10,27 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProductList {
   products = [
-    { id: 1, name: 'Laptop', price: 1000, inStock: true },
-    { id: 2, name: 'Phone', price: 500, inStock: false },
-    { id: 3, name: 'Tablet', price: 300, inStock: true },
+    {
+      id: 1,
+      name: 'Nike Air Max',
+      price: 2000000,
+      inStock: true,
+      image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/537ecfc3-8b48-4e42-9f39-746a5a2de31f/air-max-90-shoes.png'
+    },
+    {
+      id: 2,
+      name: 'Adidas UltraBoost',
+      price: 2500000,
+      inStock: false,
+      image: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/17c5fe3454474036b4fcaf6d00a04bb1_9366/Ultraboost_1.0_Shoes_White_GX3408_01_standard.jpg'
+    },
+    {
+      id: 3,
+      name: 'Converse Classic',
+      price: 900000,
+      inStock: true,
+      image: 'https://www.converse.com/on/demandware.static/-/Sites-converse-master-catalog/default/dw8f9e556d/images/a_107/1U647C_A_107X1.jpg'
+    },
   ];
 
   filterText = '';
